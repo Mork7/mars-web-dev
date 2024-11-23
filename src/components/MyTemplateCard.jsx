8e3c23ff39da61f4c9de62815277698a6ea1fa91
Mark Finerty <finertymark@gmail.com>
2024-11-23 10:12:59 -0500

    add templates 1 and 2

--- src/components/MyTemplateCard.jsx
+++ src/components/MyTemplateCard.jsx
@@ -1,12 +1,12 @@
 import { Card } from "flowbite-react";
 import PropTypes from "prop-types";
 
-function MyTemplateCard({ title, description, imgSrc }) {
+function MyTemplateCard({ title, description, imgSrc, href }) {
   return (
     <Card className="max-w-sm bg-black border border-zinc-700" imgAlt={title} imgSrc={imgSrc}>
-      <h3 className="text-2xl font-bold tracking-tight text-red-500">
+      <a href={href} className="text-2xl font-bold tracking-tight text-red-500 hover:cursor-pointer hover:underline">
         {title}
-      </h3>
+      </a>
       <p className="font-normal text-gray-400">
         {description}
       </p>
@@ -18,6 +18,7 @@ MyTemplateCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   imgSrc: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
 };
 
 export default MyTemplateCard;
--- src/sections/TemplatesSection.jsx
+++ src/sections/TemplatesSection.jsx
@@ -4,14 +4,16 @@ export default function TemplatesSection() {
   return (
     <section className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-12">
       <MyTemplateCard
-        title="Template 1"
-        description="A clean modern webpage for a small business. Features a navbar, footer, social media links and image gallaery of products."
-        imgSrc="/template.png"
+        title="Restaurant Red"
+        description="This template is for a restaurant, with an emphasis on dining. Just send us the media you wish to display and we will do the rest."
+        imgSrc="/restaurant1.png"
+        href="/templates/template1/restaurant1.html"
       />
       <MyTemplateCard
-        title="Template 2"
-        description="A clean modern webpage for a small business. Features a navbar, footer, social media links and image gallaery of products."
-        imgSrc="/template.png"
+        title="Restaurant Teal"
+        description="This is another webpage for a restaurant. With a full screen sized 'hero-section' displaying a beautiful image of your restaurant, and some text to describe your business."
+        imgSrc="/restaurant2.png"
+        href="/templates/template2/restaurant2.html"
       />
       <MyTemplateCard
         title="Template 3"
